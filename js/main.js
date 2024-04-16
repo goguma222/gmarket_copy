9e82f9e9bec7eb93b9385936099528bd5ac3a5d9
jang <janghee6960@gmail.com>
2024-04-17 01:58:47 +0900

    2024-04-16 퍼블리싱중입니다.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function category_view_layer () {
+    var category_list = document.querySelectorAll('.list-item-1depth');
+    var category_sub_list = document.querySelectorAll('.box-category-2depth-content');
+
+
+
+    console.log(category_list)
+    console.log(category_sub_list)
+}
+category_view_layer();
